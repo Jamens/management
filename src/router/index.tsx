5af62817d553994c1a4f43a379249c1cf07793e2
ZJH <211048131@qq.com>
2023-05-08 03:30:32 +0800

    fix:抽取懒加载组件

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,7 +3,10 @@ import { Navigate } from "react-router-dom";
 import React, { lazy } from "react";
 const User = lazy(() => import("../views/User"));
 const About = lazy(() => import("../views/About"));
-// 懒加载模式需要我们给他添加一个Loading组件
+// 懒加载模式需要我们给外面一层嵌套Loading提示加载组件
+const withLoadingComponent = (comp: JSX.Element) => (
+  <React.Suspense fallback={<div>Loading...</div>}>{comp}</React.Suspense>
+);
 const routes = [
   {
     path: "/",
@@ -15,19 +18,11 @@ const routes = [
   },
   {
     path: "/about",
-    element: (
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <About />
-      </React.Suspense>
-    ),
+    element: withLoadingComponent(<About />),
   },
   {
     path: "/user",
-    element: (
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <User />
-      </React.Suspense>
-    ),
+    element: withLoadingComponent(<User />),
   },
 ];
 export default routes;
