df0aec47061c1b47e606c3d95b6ee41705d2b28d
ZJH <211048131@qq.com>
2023-05-08 01:53:23 +0800

    add:types/node和配置别名

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "reset-css";
 import App from "./App.tsx";
-import "./assets/global.scss";
+import "@/assets/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
