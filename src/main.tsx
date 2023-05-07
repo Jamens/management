68230358cca8ab49230419371bb10feb8c988c4f
ZJH <211048131@qq.com>
2023-05-08 01:29:57 +0800

    add:reset-css和scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "reset-css";
 import App from "./App.tsx";
+import "./assets/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
