c81b9c0b85762d331b8986ee3481c0ce6010e9f3
ZJH <211048131@qq.com>
2023-05-11 01:12:55 +0800

    add:点击输入框获取值逻辑处理

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Input, Space, Button } from "antd";
 import styles from "./login.module.scss";
 import initLoginBg from "./init.ts";
@@ -10,6 +10,26 @@ const View = () => {
       initLoginBg();
     };
   }, []);
+  // 获取用户输入账号
+  const [userNameVal, setUserNameVal] = useState("");
+  // 获取用户输入密码
+  const [passwordVal, setPasswordVal] = useState("");
+  // 获取用户输入验证码
+  const [captchaVal, setCaptchaVal] = useState("");
+  // Input点击获取值
+  const userNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserNameVal(e.target.value);
+  };
+  const passwordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPasswordVal(e.target.value);
+  };
+  const captchValChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCaptchaVal(e.target.value);
+  };
+  // 登录按钮事件
+  const toLogin = () => {
+    console.log(userNameVal, passwordVal, captchaVal);
+  };
   return (
     <div className={styles.loginPage}>
       <canvas id="canvas" style={{ display: "block" }}></canvas>
@@ -20,10 +40,10 @@ const View = () => {
         </div>
         <div className="form">
           <Space direction="vertical" size="middle" style={{ display: "flex" }}>
-            <Input placeholder="用户们" />
-            <Input.Password placeholder="密码" />
+            <Input placeholder="用户们" onChange={userNameChange} />
+            <Input.Password placeholder="密码" onChange={passwordChange} />
             <div className="captchaBox">
-              <Input placeholder="验证码" />
+              <Input placeholder="验证码" onChange={captchValChange} />
               <div className="captchaImg">
                 <img
                   src="https://img1.baidu.com/it/u=413643897,2296924942&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1683824400&t=402b5482890ae2358143c5c0090edc6c"
@@ -32,7 +52,7 @@ const View = () => {
                 />
               </div>
             </div>
-            <Button type="primary" className="loginBtn" block>
+            <Button type="primary" className="loginBtn" block onClick={toLogin}>
               登录
             </Button>
           </Space>
